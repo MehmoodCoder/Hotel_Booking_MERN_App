ae182ef1313a618478141a191cc777771c9a38d6
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-15 14:30:52 +0500

    Add condition for the mouniting of navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar"
+import { useLocation } from "react-router-dom";
 
 function App() {
 
+  const isOwnerPath = useLocation().include("owner")
+
   return (
     <>
-      <Navbar/>
+      {!isOwnerPath && <Navbar/>}
     </>
   )
 }
