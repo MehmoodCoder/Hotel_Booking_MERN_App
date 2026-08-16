0fc05109fa55223b4d19b8eddb0227eb397a8212
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-16 18:30:21 +0500

    Resolve error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "../src/Routes/Home";
 
 function App() {
 
-  const isOwnerPath = useLocation().pathname.includes("owner");
+  const isOwnerPath = useLocation().pathname.includes("owner")
 
   return (
     <>
       {!isOwnerPath && <Navbar/>}
+      <div className="min-h-[70vh]">
+        <Routes >
+          <Route element={<Home/>} path="/" />
+        </Routes>
+      </div>
     </>
   )
 }
