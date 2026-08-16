bcf91f09ca95e212b148f7b237e14c3de2e21a55
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-16 18:39:36 +0500

    Add text to Hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const  Hero = () => {
-    return(
-        <>
-            <div className="flex flex-col items-start justify-content px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url(https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-no-repeat bg-cover bg-center h-screen">
+const Hero = () => {
+  return (
+    <>
+      <div className="flex flex-col items-start justify-content px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url(https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-no-repeat bg-cover bg-center h-screen">
+        <p>Luxury Stay Awaits You</p>
+        <h1>Discover Your Perfect Gateway</h1>
+        <p>
+          Book premium hotels, cozy resorts, and exclusive suites worldwide at
+          the guaranteed best competitive prices today.
+        </p>
+      </div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default Hero
+export default Hero;
