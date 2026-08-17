8c3ff571c13548c33981f1a77446031d28500cc4
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 21:34:36 +0500

    Mount Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../src/components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "../src/Routes/Home";
+import Footer from "./components/Footer";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route element={<Home />} path="/" />
         </Routes>
+        <Footer/>
       </div>
     </>
   );
