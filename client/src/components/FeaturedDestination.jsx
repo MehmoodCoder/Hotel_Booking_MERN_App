6d0fc6f54e44f1d7855f657fa87086eba81c2fb4
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 13:51:57 +0500

    Add data

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 import HotelCard from "./HotelCard";
 
-const data = {};
+const data = [
+  {
+    _id: "room_1",
+    img: [
+      "https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&w=800&q=80",
+    ],
+    hotel: {
+      name: "Grand Palace Resort",
+      address: "Mall Road, Murree",
+    },
+    price: 150,
+  },
+  {
+    _id: "room_2",
+    img: [
+      "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?auto=format&fit=crop&w=800&q=80",
+    ],
+    hotel: {
+      name: "Serena Hotel",
+      address: "Khayaban-e-Suhrawardy, Islamabad",
+    },
+    price: 220,
+  },
+  {
+    _id: "room_3",
+    img: [
+      "https://images.unsplash.com/photo-1611892440504-42a792e24d32?auto=format&fit=crop&w=800&q=80",
+    ],
+    hotel: {
+      name: "Pearl Continental",
+      address: "Shahrah-e-Quaid-e-Azam, Lahore",
+    },
+    price: 180,
+  },
+  {
+    _id: "room_4",
+    img: [
+      "https://images.unsplash.com/photo-1590490360182-c33d57733427?auto=format&fit=crop&w=800&q=80",
+    ],
+    hotel: {
+      name: "Luxus Hunza Hotel",
+      address: "Attabad Lake, Hunza Valley",
+    },
+    price: 260,
+  },
+];
 
 const FeaturedDestination = () => {
   return (
