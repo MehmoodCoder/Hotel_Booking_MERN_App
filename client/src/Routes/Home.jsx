cee56cda041d19c8a1a0c1be099f3e631de1fcc2
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 21:08:33 +0500

    Mount Reviews Components

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import FeaturedDestination from "../components/Featureddestination";
 import Offers from "../components/Offers";
+import Reviews from '../components/Reviews'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedDestination />
       <Offers/>
+      <Reviews />
     </>
   );
 };
