c0fa088e874060aea820dbdc4453219251501e36
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 15:01:42 +0500

    Format code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,9 +12,9 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
-      </ClerkProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
+  </BrowserRouter>,
 );
