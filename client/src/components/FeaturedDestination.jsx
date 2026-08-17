a91f256e099a4303d731274427ed591877e3a706
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 13:43:05 +0500

    Forget to add tailwind css

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -6,9 +6,9 @@ const data = {};
 const FeaturedDestination = () => {
   return (
     <>
-      <div>
-        <div>
-          {data.map((room, index) => (
+      <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
+        <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
+          {data.map((room, index) => ( 
             <HotelCard key={room._id} index={room.index} room={room} />
           ))}
         </div>
