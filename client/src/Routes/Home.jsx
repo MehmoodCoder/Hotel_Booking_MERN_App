70d44d47b856b3eb3a80e9e88a2dfbce497acddb
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 10:03:04 +0500

    Format code

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
 
-const  Home = () => {
-    return(
-        <>
-        <Hero/>
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
 
-export default Home
+export default Home;
