87ee5c218085ea84b698d62f420f02b56bfb61e7
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 14:34:07 +0500

    Align cards

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -55,8 +55,8 @@ const HotelData = [
 const FeaturedDestination = () => {
   return (
     <>
-      <div className="flex flex-col items-center w-full px-6 md:px-13 lg:px-18 bg-slate-50 py-20">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full px-4 py-8">
+      <div className="flex flex-col items-center justify-center w-full px-6 md:px-13 lg:px-18 bg-slate-50 py-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full px-4 py-8 justify-items-center">
           {HotelData.map((room, index) => (
             <HotelCard key={room._id} room={room} index={index} />
           ))}
