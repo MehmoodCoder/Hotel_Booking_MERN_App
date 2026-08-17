05b68c1294514b5dc8734c593c28363b93b913c1
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 13:52:58 +0500

    Format code

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -53,7 +53,7 @@ const FeaturedDestination = () => {
     <>
       <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
         <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
-          {data.map((room, index) => ( 
+          {data.map((room, index) => (
             <HotelCard key={room._id} index={room.index} room={room} />
           ))}
         </div>
