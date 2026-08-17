240be757e457615a83e730f24d58bc9538c8229e
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 21:17:50 +0500

    Format code

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,13 +1,11 @@
 import React from "react";
- 
+
 const Newsletter = () => {
-    return(
-        <>
-            <div className="">
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default Newsletter
+export default Newsletter;
