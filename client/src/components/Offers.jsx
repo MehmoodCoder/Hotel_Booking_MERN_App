e9dd1ca4fd12b64320672962182678b6384c4bd9
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-17 20:25:32 +0500

    Resolve navigation issue

--- client/src/components/Offers.jsx
+++ client/src/components/Offers.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Title from "./Title";
+import { useNavigate } from "react-router-dom";
 
 const Offers = () => {
+    const navigate = useNavigate()
   return (
     <>
       <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30">
@@ -14,7 +16,7 @@ const Offers = () => {
           
           <button
             onClick={() => {
-              Navigate("/offers");
+              navigate("/offers");
               scrollTo(0, 0);
             }}
             className="my-14 px-4 py-2 text-sm font-medium border border-white-100 cursor-pointer transition-all bg-gray-900 rounded hover:bg-white-100"
