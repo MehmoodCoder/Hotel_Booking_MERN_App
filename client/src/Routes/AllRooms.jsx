a032b8fc8c5007bc32a6de49c4c97e7f428d35a8
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-18 12:39:22 +0500

    Create new Route

---
+++ client/src/Routes/AllRooms.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AllRooms = () => {
+    return(
+        <>
+            <div className="">
+
+            </div>
+        </>
+    )
+}
+
+export default AllRooms
