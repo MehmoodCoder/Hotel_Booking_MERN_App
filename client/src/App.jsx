5f215081a46baac0e05672f723cd00b37b427b8e
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-18 12:48:41 +0500

    Mount new Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "../src/components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "../src/Routes/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./Routes/AllRooms";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,6 +13,7 @@ function App() {
       <div className="min-h-[70vh]">
         <Routes>
           <Route element={<Home />} path="/" />
+          <Route element={<AllRooms/>} path="/rooms"/>
         </Routes>
       </div>
       <Footer />
