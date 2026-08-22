a0f44fe39cf6a8e9c3449a4e87de365360693cc1
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-22 12:20:04 +0500

    Create layout Route with Mounting Navbar

---
+++ client/src/Routes/OwnerRoutes/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/OwnerComponents/Navbar'
+
+function Layout() {
+  return (
+    <div className='flex flex-col h-screen'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Layout
