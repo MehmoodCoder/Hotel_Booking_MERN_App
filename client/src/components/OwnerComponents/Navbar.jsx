0be44c325607f263b589ed0470f674f185046134
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-22 12:21:26 +0500

    Create Navbar component

--- client/src/components/OwnerComponents/Navbar.jsx
+++ client/src/components/OwnerComponents/Navbar.jsx
@@ -1,9 +1,17 @@
 import react from 'react'
+import { Link } from 'react-router-dom'
+import assets from '../../assets'
+import { UserButton } from '@clerk/react'
 
 const Navbar = () => {
     return (
         <>
-        <div className=""></div>
+        <div className="flex items-center justify-between px-4 md:px-8 border-b py-3 border-gray-700 bg-gray-900 transition-all duration-300">
+            <Link to= "/">
+                <img src={assets.logo} alt="Logo" className='h-9 invert opacity-80' />
+            </Link>
+            <UserButton/>
+        </div>
         </>
     )
 }
