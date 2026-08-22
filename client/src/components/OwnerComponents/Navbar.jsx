e67802f4f37f85c3095a97a350027dfc366a5a95
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-22 12:23:52 +0500

    Resolve import error

--- client/src/components/OwnerComponents/Navbar.jsx
+++ client/src/components/OwnerComponents/Navbar.jsx
@@ -1,6 +1,6 @@
-import react from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import assets from '../../assets'
+import logo from '../../assets/logo.svg'
 import { UserButton } from '@clerk/react'
 
 const Navbar = () => {
@@ -8,7 +8,7 @@ const Navbar = () => {
         <>
         <div className="flex items-center justify-between px-4 md:px-8 border-b py-3 border-gray-700 bg-gray-900 transition-all duration-300">
             <Link to= "/">
-                <img src={assets.logo} alt="Logo" className='h-9 invert opacity-80' />
+                <img src={logo} alt="Logo" className='h-9 brightness-0 invert' />
             </Link>
             <UserButton/>
         </div>
