f013948c2679aa588c0adb929e2081d3aff27cdc
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-22 13:34:57 +0500

    Resolve error

--- client/src/Routes/OwnerRoutes/Layout.jsx
+++ client/src/Routes/OwnerRoutes/Layout.jsx
@@ -5,14 +5,14 @@ import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <div className='flex flex-col h-screen'>
-        <Navbar/>
-        <div className="flex h-full">
-            <Sidebar/>
-        </div>
-        <div className="flex-1 p-4 pt-10 md:px-10 h-full">
-            <Outlet/>
-        </div>
+    <div className='flex flex-col min-h-screen bg-[#121212] text-white'>
+      <Navbar />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6 md:p-10">
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
