f11e8a85200d2244c69665761a24bde0b3e76afd
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-22 10:43:48 +0500

    Create new Route with Title

---
+++ client/src/Routes/MyBookings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../components/Title";
+
+const MyBookings = () => {
+  return (
+    <>
+      <div className="">
+        <Title align="left" title="My Bookings" description="View your upcoming and past bookings. All your reservations in one place." />
+      </div>
+    </>
+  );
+};
