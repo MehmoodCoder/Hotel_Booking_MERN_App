af58f81995342abbf970808c619248e44135ef17
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 21:50:53 +0500

    Create getOwnerRooms controller

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -60,3 +60,15 @@ export const getRooms = async (req, res) => {
   }
 };
 
+export const getOwnerRooms = async (req, res) => {
+  try {
+    const hotelData = await Hotel({ owner: req.auth.userId });
+
+    const rooms = await Room.find({ hotel: hotelData._id.toString() }).populate("hotel");
+
+    res.json({ success: true, rooms });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
