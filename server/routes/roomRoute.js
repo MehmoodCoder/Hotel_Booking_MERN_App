926474367f2b859cc20a0e7a1292d9130116e2e4
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 21:56:33 +0500

    Create protected route for getOwnerRooms

--- server/routes/roomRoute.js
+++ server/routes/roomRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import upload from '../middlewares/uploadMidddleware'
 import { protect } from '../middlewares/authMiddleware'
-import { createRoom } from '../controllers/roomController'
+import { createRoom, getOwnerRooms } from '../controllers/roomController'
 
 const Router = express.Router()
 
 Router.route('/').post( upload.array('images', 4), protect, createRoom).get(getRooms)
+Router.get('/owner', protect, getOwnerRooms)
 
 
 export default Router
