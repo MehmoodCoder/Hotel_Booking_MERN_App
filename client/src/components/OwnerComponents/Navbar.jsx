50570ccf02d74fd87e4e26c722367683b509eb37
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 07:51:23 +0500

    Change Navbar bg color

--- client/src/components/OwnerComponents/Navbar.jsx
+++ client/src/components/OwnerComponents/Navbar.jsx
@@ -6,7 +6,7 @@ import { UserButton } from '@clerk/react'
 const Navbar = () => {
     return (
         <>
-        <div className="flex items-center justify-between px-4 md:px-8 border-b py-3 border-gray-700 bg-gray-900 transition-all duration-300">
+        <div className="flex items-center justify-between px-4 md:px-8 border-b py-3 border-gray-700 bg-[#111111] transition-all duration-300">
             <Link to= "/">
                 <img src={logo} alt="Logo" className='h-9 brightness-0 invert' />
             </Link>
