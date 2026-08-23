59ddb590e2f5bc2ca49add7c02ffd39aacbe5b52
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 22:07:38 +0500

    Format code

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -1,32 +1,32 @@
 import Hotel from "../models/hotelModel";
-import User from '../models/UserModel.js'
+import User from "../models/UserModel.js";
 
-export const RegisterHotel = async (req, res ) => {
-    try {
-        const {name , address, contact, city } = req.body
-        const owner = req.user._id
+export const RegisterHotel = async (req, res) => {
+  try {
+    const { name, address, contact, city } = req.body;
+    const owner = req.user._id;
 
-        const hotel = await Hotel.findOne({})
+    const hotel = await Hotel.findOne({});
 
-        if (hotel) {
-            return res.json({
-            success: false,
-            message:  "Hotel already registered"
-        })
-        }
-
-        await Hotel.create({name, address, contact, city, owner})
-
-        await User.findByIdAndUpdate(owner, {role: "hotelOwner"})
-
-        res.json({
-            success: true,
-            message:  "Hotel registered successfully"
-        })
-    } catch (e) {
-        res.json({
-            success: false,
-            message:  e.message
-        })
+    if (hotel) {
+      return res.json({
+        success: false,
+        message: "Hotel already registered",
+      });
     }
-}
+
+    await Hotel.create({ name, address, contact, city, owner });
+
+    await User.findByIdAndUpdate(owner, { role: "hotelOwner" });
+
+    res.json({
+      success: true,
+      message: "Hotel registered successfully",
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      message: e.message,
+    });
+  }
+};
