aa13e8132b5b5cd2ea0494015076e9103e862c93
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 13:29:21 +0500

    Format code

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,16 +1,15 @@
-import User from '../models/UserModel.js'
+import User from "../models/UserModel.js";
 
 export const protect = async (req, res, next) => {
-    const {userId} = req.auth
-    if (!userId) {
-        return res.json({
-            success: false,
-            message: "Not Authenticated"
-        })
-    } else{
-        const user = User.findById(userId)
-        req.user = user
-        next()
-    }
-}
-
+  const { userId } = req.auth;
+  if (!userId) {
+    return res.json({
+      success: false,
+      message: "Not Authenticated",
+    });
+  } else {
+    const user = User.findById(userId);
+    req.user = user;
+    next();
+  }
+};
