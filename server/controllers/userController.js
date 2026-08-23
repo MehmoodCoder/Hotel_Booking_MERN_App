be41e988c05ce62e41e11d618b13b45a9c7fc549
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 13:38:06 +0500

    Create storeRecentSearchCities controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,3 +14,27 @@ export const getUserData = async (req, res) => {
     });
   }
 };
+
+export const storeRecentSearchCities = async (req, res) => {
+    try {
+        const {recentSearchedCities} = req.body
+        const user = await req.user
+        if (user.recentSearchedCities.length < 3) {
+            user.recentSearchedCities.push(recentSearchedCities)
+        } else {
+            user.recentSearchedCities.shift()
+            user.recentSearchedCities.push(recentSearchedCities)
+        }
+
+        await user.save()
+        res.json({
+            success: true,
+            message: "City added"
+        })
+    } catch (e) {
+        res.json({
+            success: false,
+            message: e.message
+        })
+    }
+}
