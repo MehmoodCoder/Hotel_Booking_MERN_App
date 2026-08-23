79f3ed282dc6185d894fcb53e1a3e20a57bd3d3b
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 19:47:45 +0500

    Create Route for storeRecentSearchCities

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { protect } from '../middlewares/authMiddleware.js'
-import { getUserData } from '../controllers/userController.js'
+import { getUserData, storeRecentSearchCities } from '../controllers/userController.js'
 
 const Router = express.Router()
 
 Router.get('/', protect, getUserData)
+Router.get('/store-recent-search', protect, storeRecentSearchCities)
 
 export default Router
