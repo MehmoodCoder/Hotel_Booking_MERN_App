13141d4ab781a5adb6fb55cee5c481c254beca10
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 12:31:51 +0500

    remove async

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json);
 app.use(clerkMiddleware());
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is working");
 });
 app.use("/api/clerk", clerkWebhooks);
