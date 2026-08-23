67eec000c533efbc2001ce79532e21d2fbf05741
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 21:35:58 +0500

    Create route for createRoom

---
+++ server/routes/roomRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import upload from '../middlewares/uploadMidddleware'
+import { protect } from '../middlewares/authMiddleware'
+import { createRoom } from '../controllers/roomController'
+
+const Router = express.Router()
+
+Router.post('/', upload.array('images', 4), protect, createRoom)
+
+
+export default Router
