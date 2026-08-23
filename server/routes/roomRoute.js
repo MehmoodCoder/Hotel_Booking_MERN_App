0df8b6ad5392c91635781a4261c221e06d0d3571
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 21:57:54 +0500

    Create route for toggleRoomAvailability

--- server/routes/roomRoute.js
+++ server/routes/roomRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import upload from '../middlewares/uploadMidddleware'
 import { protect } from '../middlewares/authMiddleware'
-import { createRoom, getOwnerRooms } from '../controllers/roomController'
+import { createRoom, getOwnerRooms, toggleRoomAvailability } from '../controllers/roomController'
 
 const Router = express.Router()
 
 Router.route('/').post( upload.array('images', 4), protect, createRoom).get(getRooms)
 Router.get('/owner', protect, getOwnerRooms)
-
+Router.get('/toggle-availability', protect, toggleRoomAvailability)
 
 export default Router
