019771cba2d7734c1334690733a6941012681107
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 13:28:50 +0500

    Create Protect Middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,16 @@
+import User from '../models/UserModel.js'
+
+export const protect = async (req, res, next) => {
+    const {userId} = req.auth
+    if (!userId) {
+        return res.json({
+            success: false,
+            message: "Not Authenticated"
+        })
+    } else{
+        const user = User.findById(userId)
+        req.user = user
+        next()
+    }
+}
+
