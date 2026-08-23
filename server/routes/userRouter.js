703566abe506f3259a2fbcdfa39fa2524bd8cf52
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 19:48:10 +0500

    Format code

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,13 @@
-import express from 'express'
-import { protect } from '../middlewares/authMiddleware.js'
-import { getUserData, storeRecentSearchCities } from '../controllers/userController.js'
+import express from "express";
+import { protect } from "../middlewares/authMiddleware.js";
+import {
+  getUserData,
+  storeRecentSearchCities,
+} from "../controllers/userController.js";
 
-const Router = express.Router()
+const Router = express.Router();
 
-Router.get('/', protect, getUserData)
-Router.get('/store-recent-search', protect, storeRecentSearchCities)
+Router.get("/", protect, getUserData);
+Router.get("/store-recent-search", protect, storeRecentSearchCities);
 
-export default Router
+export default Router;
