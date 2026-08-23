035feb2b929d1c5ee9bbf476197fc8b2a795c595
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-23 11:26:37 +0500

    Format code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv, { config } from "dotenv";
 import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 config.dotenv();
@@ -12,10 +12,10 @@ const app = express();
 const PORT = process.env.PORT || "6000";
 
 app.use(cors());
-app.use(express.json)
-app.use(clerkMiddleware())
+app.use(express.json);
+app.use(clerkMiddleware());
 
-app.use('/api/clerk', clerkWebhooks)
+app.use("/api/clerk", clerkWebhooks);
 
 app.listen(PORT, () => {
   console.log(`The server is running on PORT : ${PORT}`);
