2bed11a5ab86527767abdf95cc7cb5e8281252e4
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 15:02:42 +0500

    Create route for CreateBooking

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -5,5 +5,7 @@ import protect from '../middlewares/authMiddleware.js'
 const Router = express.Router()
 
 Router.post('/check-availability', checkAvailabilityAPI)
+Router.post("/book", protect, CreateBooking)
+
 
 export default Router
