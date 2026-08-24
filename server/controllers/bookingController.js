aba4bc2a5eee6440df5b995ea1b1ce87dc5b70e6
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 14:45:35 +0500

    Create controller for CreateBooking

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,5 @@
 import Booking from "../models/bookingModel"
+import Room from "../models/roomModel"
 
 
 const checkAvailability = async ({  checkInDate, checkOutDate, room }) => {
@@ -30,3 +31,48 @@ export const checkAvailabilityAPI = async (req, res) => {
         })
     }
 }
+
+
+export const CreateBooking = async (req, res) => {
+    try {
+        const {room, checkInDate, checkOutDate, guest} = req.body
+        const user = req.body._id
+        const isAvailable = await checkAvailability({checkInDate, checkOutDate, room})
+
+        if (!isAvailable) {
+            res.json({
+            success: false,
+            msg: "room is not Available"
+        })
+        const RoomData = await Room.findById(room).populate("hotel")
+        let price = RoomData.pricePerNight
+
+        const CheckIn = new Date(checkInDate)
+        const CheckOut = new Date(checkOutDate)
+        const timeDif = CheckOut.getTime() - checkInDate.getTime()
+        const nights = Math.ceil(timeDif/ (1000 * 3600 * 24))
+
+        totalPrice *= nights
+
+        const booking = await Booking.create({
+            user,
+            room,
+            hotel: RoomData.hotel._id,
+            guests: +guest,
+            checkInDate,
+            checkOutDate,
+            totalPrice
+        })
+        }
+        res.json({
+            success: true,
+            msg: "Booking create successfully"
+        })
+    } catch (e) {
+        res.json({
+            success: false,
+            msg: e.msg
+        })
+    }
+}
+
