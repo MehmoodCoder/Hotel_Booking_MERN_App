2d6f6db13e32c549e4b99a3dafc402eef4b878f7
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 16:18:51 +0500

    Import ans use AppContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/react";
+import { AppProvider } from "./context/AppContext.js";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </ClerkProvider>
   </BrowserRouter>,
 );
