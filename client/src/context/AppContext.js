ed626be0b6414d651ab562521d737407f38216dc
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 16:16:16 +0500

    Create complete context

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,2 +1,65 @@
-import axios from 'axios'
+import React, { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser, useAuth } from "@clerk/clerk-react";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const currency = process.env.VITE_CURRENCY || "$";
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
+  const [isOwner, setIsOwner] = useState(false);
+  const [showHotelReg, setShowHotelReg] = useState(false);
+  const [searchCities, setSearchCities] = useState([]);
+
+  const fetchUser = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get("/api/user", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setIsOwner(data.role === "hotelOwner");
+        setSearchCities(data.recentSearchedCities)
+      }else{
+        setTimeout(() => {
+            fetchUser()
+        }, 5000)
+      }
+    } catch (error) {
+      toast.error(e.message)
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchUser();
+    }
+  }, [user]);
+
+  const value = {
+    currency,
+    navigate,
+    user,
+    getToken,
+    isOwner,
+    setIsOwner,
+    axios,
+    showHotelReg,
+    setShowHotelReg,
+    searchCities,
+    setSearchCities
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
