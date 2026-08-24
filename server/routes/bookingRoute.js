2fd9f55263d5973bd307b9c3a5d11c2669f387d2
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 15:04:40 +0500

    Create route for getuserBooking

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { checkAvailabilityAPI, CreateBooking } from '../controllers/bookingController'
+import { checkAvailabilityAPI, CreateBooking, getHotelBookings, getUserBookings } from '../controllers/bookingController'
 import protect from '../middlewares/authMiddleware.js'
 
 const Router = express.Router()
 
 Router.post('/check-availability', checkAvailabilityAPI)
 Router.post("/book", protect, CreateBooking)
-
+Router.get('/user', protect, getUserBookings)
 
 export default Router
