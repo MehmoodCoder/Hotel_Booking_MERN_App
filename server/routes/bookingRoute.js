b942766d6ec3e66dca3c41fe7c2831aafd0549dd
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 15:05:45 +0500

    Format code

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -1,12 +1,17 @@
-import express from 'express'
-import { checkAvailabilityAPI, CreateBooking, getHotelBookings, getUserBookings } from '../controllers/bookingController'
-import protect from '../middlewares/authMiddleware.js'
+import express from "express";
+import {
+  checkAvailabilityAPI,
+  CreateBooking,
+  getHotelBookings,
+  getUserBookings,
+} from "../controllers/bookingController";
+import protect from "../middlewares/authMiddleware.js";
 
-const Router = express.Router()
+const Router = express.Router();
 
-Router.post('/check-availability', checkAvailabilityAPI)
-Router.post("/book", protect, CreateBooking)
-Router.get('/user', protect, getUserBookings)
-Router.get('/hotel', protect, getHotelBookings)
+Router.post("/check-availability", checkAvailabilityAPI);
+Router.post("/book", protect, CreateBooking);
+Router.get("/user", protect, getUserBookings);
+Router.get("/hotel", protect, getHotelBookings);
 
-export default Router
+export default Router;
