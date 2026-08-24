7e0132a7c3ab7fb8f2abad50b293f9263be4fa0b
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-24 14:56:32 +0500

    Create getHotelBookings controller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -94,5 +94,29 @@ export const getUserBookings = async (req, res) => {
   }
 };
 
+export const getHotelBookings = async (req, res) => {
+  try {
+    const hotel = await Hotel.findOne({ owner: req.auth.userId });
+    if (!hotel) {
+      return res.json({ success: false, message: "No Hotel found" });
+    }
 
+    const bookings = await Booking.find({ hotel: hotel._id })
+      .populate("room hotel user")
+      .sort({ createdAt: -1 });
+
+    const totalBookings = bookings.length;
+    const totalRevenue = bookings.reduce(
+      (acc, booking) => acc + booking.totalPrice,
+      0
+    );
+
+    res.json({
+      success: true,
+      dashboardData: { totalBookings, totalRevenue, bookings },
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
