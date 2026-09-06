bd535c84a2b1ab0531c62d61e3f7bda2f3e72b16
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-06 21:43:23 +0500

    Adding console to track error

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,10 +2,12 @@ import User from "../models/UserModel.js";
 
 export const protect = async (req, res, next) => {
   const { userId } = req.auth;
+  console.log("Error in middleware");
+  
   if (!userId) {
     return res.json({
       success: false,
-      message: "Not Authenticated",
+      message: "not Authenticated",
     });
   } else {
     const user = User.findById(userId);
