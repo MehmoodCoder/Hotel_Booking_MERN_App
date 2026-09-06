994ed21551a3c7cc6cfcf7f6c1619fca8034be5e
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-06 22:06:43 +0500

    Trying to resolve error

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,15 +1,30 @@
 import User from "../models/UserModel.js";
 
 export const protect = async (req, res, next) => {
-  const { userId } = req.auth;  
-  if (!userId) {
-    return res.json({
-      success: false,
-      message: "Not Authenticated",
-    });
-  } else {
-    const user = User.findById(userId);
+  try {
+    const userId = req.auth?.userId;  
+
+    if (!userId) {
+      return res.json({
+        success: false,
+        message: "Not Authenticated",
+      });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found in database",
+      });
+    }
+
     req.user = user;
     next();
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
   }
 };
