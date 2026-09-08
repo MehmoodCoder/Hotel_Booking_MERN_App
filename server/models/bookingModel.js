9914174a04dbb2df9b0f9524823ab02477af7ed6
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-08 22:29:05 +0500

    Update Schema

--- server/models/bookingModel.js
+++ server/models/bookingModel.js
@@ -5,17 +5,23 @@ const BookingSchema = new mongoose.Schema(
     user: { type: String, required: true, ref: "User" },
     room: { type: String, required: true, ref: "Room" },
     hotel: { type: Number, required: true, ref: "Hotel" },
-    checkInDate: { type: Date, required: true, ref: "User" },
-    checkOutDate: { type: Date, required: true, ref: "User" },
+    checkInDate: { type: Date, required: true },
+    checkOutDate: { type: Date, required: true },
     price: { type: Number, required: true },
-    guests: { type: String, required: true},
-    status: { type: String, required: true, enum: ['pending', 'confirmed', 'cancelled'] },
-    paymentMethod: {type: String, default: "Pay at Hotel" , required: true},
-    isPaid: {type: Boolean, default: false}
+    guests: { type: String, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "pending",
+    },
+    paymentMethod: { type: String, default: "Pay at Hotel", required: true },
+    isPaid: { type: Boolean, default: false },
   },
   { timestamps: true },
 );
 
-const Booking = mongoose.model("Booking", BookingSchema);
+const Booking =
+  mongoose.models.Booking || mongoose.model("Booking", BookingSchema);
 
 export default Booking;
