fb27924741c032b92509c020ee242fc2defde622
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-08 21:20:46 +0500

    Remove commits

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -8,9 +8,6 @@ const connectCloudinary = async () => {
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
     })
-    console.log("Cloudinary connected successfully", process.env.CLOUDINARY_CLOUD_NAME);
-    console.log("Cloudinary API Key:", process.env.CLOUDINARY_API_KEY);
-    console.log("Cloudinary API Secret:", process.env.CLOUDINARY_API_SECRET);
 }
 
 export default connectCloudinary;
