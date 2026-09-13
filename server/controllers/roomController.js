317287214c2930f5653bbf33400ac640e1ccaf6c
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-13 14:34:35 +0500

    Update Controller

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -5,13 +5,9 @@ import Room from "../models/roomModel.js";
 export const createRoom = async (req, res) => {
   try {
     const { roomType, pricePerNight, aminities } = req.body;
-    const userId = req.auth?.userId;
-    
-    let hotel = await Hotel.findOne({ owner: userId });
+    const userId = req.auth?.userId || req.user?._id;
 
-    if (!hotel && req.user?._id) {
-      hotel = await Hotel.findOne({ owner: req.user._id });
-    }
+    let hotel = await Hotel.findOne({ owner: userId });
 
     if (!hotel) {
       return res.json({
@@ -59,9 +55,11 @@ export const getRooms = async (req, res) => {
     const rooms = await Room.find({ isAvailable: true })
       .populate({
         path: "hotel",
+        strictPopulate: false,
         populate: {
           path: "owner",
           select: "image",
+          strictPopulate: false,
         },
       })
       .sort({ createdAt: -1 });
@@ -74,18 +72,17 @@ export const getRooms = async (req, res) => {
 
 export const getOwnerRooms = async (req, res) => {
   try {
-    const userId = req.auth?._id;
+    const userId = req.auth?.userId || req.user?._id;
     let hotelData = await Hotel.findOne({ owner: userId });
-
-    if (!hotelData && req.user?._id) {
-      hotelData = await Hotel.findOne({ owner: req.user._id });
-    }
 
     if (!hotelData) {
       return res.json({ success: false, message: "No Hotel found", rooms: [] });
     }
 
-    const rooms = await Room.find({ hotel: hotelData._id }).populate("hotel");
+    const rooms = await Room.find({ hotel: hotelData._id }).populate({
+      path: "hotel",
+      strictPopulate: false,
+    });
 
     res.json({ success: true, rooms });
   } catch (error) {
