f8d87a72f4d6d77195de1c50cdd2e3b51c7decad
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-14 15:19:49 +0500

    Resolve import error

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
-import FeaturedDestination from "../components/Featureddestination";
+import FeaturedDestination from "../components/FeaturedDestination";
 import Offers from "../components/Offers";
 import Reviews from "../components/Reviews";
 import Newsletter from "../components/Newsletter";
