61271ae85cc0584ff5bc9fdfa1719072e489ce45
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-19 18:59:46 +0500

    Format code

--- client/src/components/OwnerComponents/Navbar.jsx
+++ client/src/components/OwnerComponents/Navbar.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
-import { UserButton } from '@clerk/react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import { UserButton } from "@clerk/react";
 
 const Navbar = () => {
-    return (
-        <>
-        <div className="flex items-center justify-between px-4 md:px-8 border-b py-3 border-gray-700 bg-[#111111] transition-all duration-300">
-            <Link to= "/">
-                <img src={logo} alt="Logo" className='h-9 brightness-0 invert' />
-            </Link>
-            <UserButton/>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex items-center justify-between px-4 md:px-8 border-b py-3 border-gray-700 bg-[#111111] transition-all duration-300">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="h-9 brightness-0 invert" />
+        </Link>
+        <UserButton />
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
